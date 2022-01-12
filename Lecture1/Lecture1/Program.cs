15ec8ba96b2f7fb2e2357f838b3e692cf3d6c158
Nicole Glidden <nicole.glidden@IntelliTect.com>
2022-01-12 15:31:02 -0800

    hi Jenny test

---
+++ Lecture1/Lecture1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System:
+
+class Program
+{
+    void Main()
+    {   
+        string text = "Inigo Montoya";
+        Guid guid = Guid.NewGuid();
+
+        Console.WriteLine("Hello, Jenny!");
+        Console.WriteLine("Inigo Montoya" + text);
+    }
+
+
+}
+
