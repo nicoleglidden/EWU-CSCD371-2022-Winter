e64ec559ff05f78f214aa5945defc582cb469822
Nicole Glidden <nicole.glidden@IntelliTect.com>
2022-02-02 17:59:46 -0800

    Assignment 2 BaseLoggerMixIns

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -7,33 +7,97 @@ namespace Logger.Tests
     [TestClass]
     public class BaseLoggerMixinsTests
     {
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
+
         public void Error_WithNullLogger_ThrowsException()
-        {
-            // Arrange
-
-            // Act
-            //BaseLoggerMixins.Error(null, "");
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Error_WithData_LogsMessage()
         {
             // Arrange
             var logger = new TestLogger();
 
             // Act
-            //logger.Error("Message {0}", 42);
-
+            BaseLoggerMixins.Error(logger, null!);
             // Assert
+        }
+            
+        [TestMethod]
+        public void Error_WithData_LogsMessage()
+        {
+            var logger = new TestLogger();
+
+            logger.Error("Message {0}", 42);
+
             Assert.AreEqual(1, logger.LoggedMessages.Count);
             Assert.AreEqual(LogLevel.Error, logger.LoggedMessages[0].LogLevel);
             Assert.AreEqual("Message 42", logger.LoggedMessages[0].Message);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Warning_WithNullMessage_ThrowsException()
+        {
+            var logger = new TestLogger();
+
+            BaseLoggerMixins.Warning(logger, null!);
+        }
+
+        [TestMethod]
+        public void Warning_WithData_LogsMessage()
+        {
+            var logger = new TestLogger();
+
+            logger.Warning("Message {0}", 42);
+
+            Assert.AreEqual(1, logger.LoggedMessages.Count);
+            Assert.AreEqual(LogLevel.Warning, logger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 42", logger.LoggedMessages[0].Message);
+        }
+
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Information_WithNullMessage_ThrowsException()
+        {
+            var logger = new TestLogger();
+
+            BaseLoggerMixins.Information(logger, null!);
+        }
+
+        [TestMethod]
+        public void Information_WithData_LogsMessage()
+        {
+            var logger = new TestLogger();
+
+            logger.Information("Message {0}", 42);
+
+            Assert.AreEqual(1, logger.LoggedMessages.Count);
+            Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 42", logger.LoggedMessages[0].Message);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Debug_WithNullMessage_ThrowsException()
+        {
+            var logger = new TestLogger();
+
+            BaseLoggerMixins.Debug(logger, null!);
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Debug_WithData_LogsMessage()
+        {
+            var logger = new TestLogger();
+
+            logger.Debug("Message {0}", 42);
+
+            Assert.AreEqual(1, logger.LoggedMessages.Count);
+            Assert.AreEqual(LogLevel.Debug, logger.LoggedMessages[0].LogLevel);
+            Assert.AreEqual("Message 42", logger.LoggedMessages[0].Message);
+        }
     }
 
     public class TestLogger : BaseLogger
--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,7 +1,49 @@
-﻿namespace Logger
+﻿using System;
+using System.Diagnostics;
+
+
+namespace Logger
 {
     public static class BaseLoggerMixins
     {
-        
+        public static void Error(this BaseLogger logger, string message, params object[] args)
+        {
+            CheckLoggerExists(logger);
+            message = String.Format(message, args);
+
+            logger.Log(LogLevel.Error, message);
+        }
+
+        public static void Warning(this BaseLogger logger, string message, params object[] args)
+        {
+            CheckLoggerExists(logger);
+            message= String.Format(message, args);
+
+            logger.Log(LogLevel.Warning, message);
+        }
+
+        public static void Information(this BaseLogger logger, string message, params object[] args)
+        {
+            CheckLoggerExists(logger);
+            message = String.Format(message, args);
+
+            logger.Log(LogLevel.Information, message);
+        }
+
+        public static void Debug(this BaseLogger logger, string message, params object[] args)
+        {
+            CheckLoggerExists(logger);
+            message = String.Format(message, args);
+
+            logger.Log(LogLevel.Debug, message);
+        }
+        private static void CheckLoggerExists(BaseLogger logger)
+        {
+            if (logger == null)
+            {
+                throw new ArgumentNullException("Logger cannot be null: {0}", nameof(logger));
+            }
+        }
+
     }
 }
