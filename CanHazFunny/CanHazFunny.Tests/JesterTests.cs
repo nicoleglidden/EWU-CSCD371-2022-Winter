6f6b7195e80203a8ecdc7e889ade986fc0609493
Nicole Glidden <nicole.glidden@IntelliTect.com>
2022-02-10 21:19:41 -0800

    JesterTests updates

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,11 +1,54 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
+using System.Net.Http;
+
 
 namespace CanHazFunny.Tests
 {
     [TestClass]
     public class JesterTests
     {
+        public class TestingJokeService : IJokeService
+        {
+            private HttpClient HttpClient { get; } = new();
+            public string GetJoke()
+            {
+                string joke = "Why did the chicken cross the road?";
+                return joke;
+            }
+        }
+
+        public void JesterTests()
+        {
+
+        }
+       
+        
+
+        public IJokeOutput JokeOutput { get; set; }
+        public IJokeService JokeService { get; set; }
+
+        public JesterTests(IJokeService inputJokeService, IJokeOutput inputJokeOutput)
+        {
+            JokeService = inputJokeService;
+            JokeOutput = inputJokeOutput;
+        }
+
+
+        [TestMethod]
+        public void TestingJesterService_GetJoke_ReturnsJoke()
+        {
+            IJokeService service = new TestingJokeService();
+            service.GetJoke();
+            string joke = "";
+
+            JokeOutput.WriteJoke(joke);
+
+            Assert.AreEqual(joke, service.GetJoke());
+
+
+        }
         
 
         
