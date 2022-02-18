b08bd3e27049e28ca34ed6131b60088eb0129674
Nicole Glidden <nicole.glidden@IntelliTect.com>
2022-02-17 18:23:28 -0800

    Append and Exists updated

---
+++ GenericsHomework/Node.Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Node.Tests
+{
+	[TestClass]
+	public class Class1
+	{
+		[TestMethod]
+		public void 
+		{
+		}
+	}
+}
---
+++ GenericsHomework/Node.cs
@@ -0,0 +1,77 @@
+﻿namespace GenericsHomework
+{
+    public class Node<T> 
+    {
+        public Node(T TValue)
+        {
+            this.Value = TValue;
+            this.Next = this;
+            
+        }
+
+        public T Value { get; set;}
+
+        public override string ToString()
+        {
+            return TValue.ToString();
+        }
+
+        public Node<T> Next { get; private set;}
+
+        public void Append(T TValue)
+        {
+          
+            if (this.Exists(TValue))
+            {
+                throw new ArgumentException ($"Duplicate Value not permitted:{nameof(TValue)}")
+            }
+
+            Node<T> node = new Node<T> (TValue);
+            this.Next = node; 
+
+
+        }
+
+        public void Clear()
+        {
+            var nextNode = this.Next;
+            var currentNode = this;
+
+            while (nextNode != this)
+            {
+               currentNode = nextNode;
+               nextNode = currentNode.Next;
+               currentNode.Next = currentNode;
+            }
+
+            this.Next = this; 
+            // Given a circular list we don't need to worry about garbage collection, as we've made all Nodes past the current Node unreachable by having it refer back to itself. No doubley circular linked lists please :(
+        }
+
+        public bool Exists(T TValue)
+        {
+            var nextNode = this.Next;
+            var currentNode = this;
+
+            if (this.Value == TValue)
+            {
+                return true;
+            }
+            
+            
+            while (currentNode != nextNode.Next)
+            {
+                if (nextNode.Value == TValue)
+                {
+                    return true;
+                }
+                nextNode = currentNode.Next;
+                // not at end of list
+
+            }
+
+            return false;
+        }
+
+    }
+}
